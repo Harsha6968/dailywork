586df3d3d5cd3cb7f17ebe2361acd4ccd45e351d
harsha6968 <goneharshavardhan96962@gmail>
2022-12-14 18:35:43 +0530

    okie

---
+++ inheritance/three.js
@@ -0,0 +1,8 @@
+class A{
+    qualities="good"
+    }
+    class B extends A{
+        qualities="bad"//overridding concept=>here good will be over ride as bad
+}
+    let a1=new B()
+    console.log(a1)
---
+++ inheritance/two.js
@@ -0,0 +1,10 @@
+class A{
+get_Data(){
+    console.log("hi good morning")
+}
+}
+class B extends A{//extends means inheriting the data from classA to classB without writing tha data in classB
+
+}
+let a1=new B()
+a1.get_Data()
